4fbc7ec02cf2beac2adfa0baa186cc74f5198b1f
Michelle Tran <michelletraan@hotmail.com>
2020-09-17 22:49:03 +0000

    take until initial commit

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`😜😜Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const takeUntil = function(arr, callback) {
+  let result = []
+  for (let element of arr) {
+    if (!callback(element)) {
+      result.push(element);
+    } else {
+      return result;
+    }
+  }
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
